ad58e9ba3af3c380e65214987df9bafcf8b9ac7f
Eban451 <eban316@gmail.com>
2023-04-13 13:58:06 -0400

    aaa

--- routes/routes.js
+++ routes/routes.js
@@ -2,19 +2,50 @@ import { Router } from "express";
 import fs from "fs";
 import bodyParser from "body-parser";
 import passport from "passport";
-import PassportLocal from "passport-local"
 import cookieParser from "cookie-parser";
 import session from "express-session";
+import pg from 'pg';
+import bcrypt from 'bcrypt';
+import pgPromise from 'pg-promise';
+import passportLocal from 'passport-local';
+
+
+import { Strategy as LocalStrategy } from 'passport-local';
+
+
+
+
+const { Pool } = pg;
+
+
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "puntos", //nombre de database
+    password: "12345",
+    port: 5432
+
+})
 
 import {leerArchivo} from "../public/js/functions.js" //Función para leer archivos almacenados en local
 
 //Constantes
+const pgp = pgPromise();
+const db = pgp('postgres://postgres:12345@localhost:5432/puntos');
 const router = Router()
-const PassPortLocal = PassportLocal.Strategy //Middleware usado para crear una estrategia de autenticación
-const rutaUsuarios = "BD/usuarios.json" // Ruta a json con los usuarios "registrados"
-const usuarios = await leerArchivo(rutaUsuarios) //Lectura y asignación json a variable usuarios
+//Middleware usado para crear una estrategia de autenticación
+// const rutaUsuarios = "BD/usuarios.json" // Ruta a json con los usuarios "registrados"
+// const usuarios = await leerArchivo(rutaUsuarios) //Lectura y asignación json a variable usuarios
 //Variable
 let autenticacion = false;
+
+class User {
+    constructor(id, username, password) {
+      this.id = id;
+      this.username = username;
+      this.password = password;
+    }
+  }
 
 
 router.use(bodyParser.urlencoded({extended: true}))
@@ -31,48 +62,99 @@ router.use(session({
 router.use(passport.initialize())
 router.use(passport.session())
 
-passport.use(new PassPortLocal(function(username,password,done){
-    for (let index = 0; index < usuarios.length; index++) {
-        let element = usuarios[index];
-        if(username == element.correo && password == element.password){
-            return done(null,{id:element.id, name:element.nombre})
+passport.use(
+    new LocalStrategy(
+      {
+        usernameField: 'username', // Specify the field name for the username in the request
+        passwordField: 'password', // Specify the field name for the password in the request
+      },
+      async (username, password, done) => {
+        try {
+          // Find user by username in the database
+          const user = await db.oneOrNone('SELECT * FROM users WHERE username = $1', username);
+  
+          if (!user) {
+            // If user not found, return error
+            return done(null, false, { message: 'Incorrect username.' });
+          }
+  
+          if (user.password !== password) {
+            // If password doesn't match, return error
+            return done(null, false, { message: 'Incorrect password.' });
+          }
+  
+          // If username and password match, return user object
+          return done(null, user);
+        } catch (err) {
+          // If any error occurs, return error
+          return done(err);
         }
-    }
-    return done(null,false)
-}))
-
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-    done(null,{id})
-});
+      }
+    )
+  );
 
 
 router.get("/", (req, res) =>{
     res.render("index",{"titulo": "Página Personal"})
 })
 
-router.get("/carto", (req,res,next) =>{  
+import { ensureAuthenticated } from '../middlewares/auth'; // Import a custom middleware to ensure authentication
+
+// Modify the route to use passport-local
+router.get('/carto', ensureAuthenticated, (req, res) => {
+  // Render the "carto" view if the user is authenticated
+  res.render('carto', { autenticacion: true });
+});
+
+
+// router.get("/carto", (req,res,next) =>{  
                                 
-    if(req.isAuthenticated()){ //Si ya está autenticado seguira al siguiente parámetro que ingresemos a router.get()
-        autenticacion = true
-        return next()
-    }else{
-        res.redirect("/login") //Si aún no está autenticado, redirigirá a "login"
-    }
-},
-(req, res) =>{ //Con las comprobaciones anteriores exitosas pasa a renderizar la vista "carto"
-    res.render("carto",{autenticacion})
-}
-)
+//     if(req.isAuthenticated()){ //Si ya está autenticado seguira al siguiente parámetro que ingresemos a router.get()
+//         autenticacion = true
+//         return next()
+//     }else{
+//         res.redirect("/login") //Si aún no está autenticado, redirigirá a "login"
+//     }
+// },
+// (req, res) =>{ //Con las comprobaciones anteriores exitosas pasa a renderizar la vista "carto"
+//     res.render("carto",{autenticacion})
+// }
+// )
 
 router.get("/login", (req, res) =>{
     res.render("login")
 })
 
-router.post("/login",passport.authenticate("local",{successRedirect: "/carto",failureRedirect: "/login"}))
+// LOGIN FUNCIONALIDAD
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Retrieve user from the database by email
+    pool.query('SELECT * FROM usuarios WHERE email = $1', [email], (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error logging in');
+        } else if (result.rows.length === 0) {
+            res.render('login', { error: 'Invalid email or password' });
+        } else {
+            const hashedPassword = result.rows[0].password;
+
+            // Compare the provided password with the hashed password
+            bcrypt.compare(password, hashedPassword, (err, isMatch) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).send('Error logging in');
+                } else if (isMatch) {
+                    res.send(`Welcome ${email}!`);
+                } else {
+                    res.render('login', { error: 'Invalid email or password' });
+                }
+            });
+        }
+    });
+});
 
 router.get("/registro", (req, res) =>{
     res.render("registro")
