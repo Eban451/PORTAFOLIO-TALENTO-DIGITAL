a11348bed367f02f9c5ee94ebb73c9a6b67b8ece
Eban451 <eban316@gmail.com>
2023-04-25 01:48:42 -0400

    BIEN

--- passportConfig.js
+++ passportConfig.js
@@ -5,39 +5,39 @@ import bcrypt from "bcrypt";
 const initialize = (passport) => {
   console.log("Initialized");
 
-  const authenticateUser = (email, password, done) => {
+  const fetchUser = async (email) => {
+    const response = await fetch(`http://localhost:4000/api/v1/users2?email=${email}`);
+    const data = await response.json();
+    return data[0];
+  }
+  
+  const authenticateUser = async (email, password, done) => {
     console.log(email, password);
-    pool.query(
-      `SELECT * FROM users WHERE email = $1`,
-      [email],
-      (err, results) => {
-        if (err) {
-          throw err;
-        }
-        console.log(results.rows);
-
-        if (results.rows.length > 0) {
-          const user = results.rows[0];
-
-          bcrypt.compare(password, user.password, (err, isMatch) => {
-            if (err) {
-              console.log(err);
-            }
-            if (isMatch) {
-              return done(null, user);
-            } else {
-              //password is incorrect
-              return done(null, false, { message: "Password is incorrect" });
-            }
-          });
-        } else {
-          // No user
-          return done(null, false, {
-            message: "No user with that email address",
-          });
-        }
+    try {
+      const user = await fetchUser(email);
+  
+      if (user) {
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if (err) {
+            console.log(err);
+          }
+          if (isMatch) {
+            return done(null, user);
+          } else {
+            //password is incorrect
+            return done(null, false, { message: "Password is incorrect" });
+          }
+        });
+      } else {
+        // No user
+        return done(null, false, {
+          message: "No user with that email address",
+        });
       }
-    );
+    } catch (err) {
+      console.error(err);
+      done(err);
+    }
   };
 
   passport.use(
@@ -55,14 +55,16 @@ const initialize = (passport) => {
   // In deserializeUser that key is matched with the in memory array / database or any data resource.
   // The fetched object is attached to the request object as req.user
 
-  passport.deserializeUser((id, done) => {
-    pool.query(`SELECT * FROM users WHERE id = $1`, [id], (err, results) => {
-      if (err) {
-        return done(err);
-      }
-      console.log(`ID is ${results.rows[0].id}`);
-      return done(null, results.rows[0]);
-    });
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const response = await fetch(`http://localhost:4000/api/v1/users/${id}`);
+      const user = await response.json();
+      console.log(`ID is ${user.id}`);
+      done(null, user);
+    } catch (err) {
+      console.error(err);
+      done(err);
+    }
   });
 };
 
