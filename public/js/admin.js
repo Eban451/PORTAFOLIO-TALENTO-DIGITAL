e13125bbf9b09c1ba5546d91d61d92f704b90619
Esteban <eban316@gmail.com>
2023-05-08 12:39:58 -0400

    NuevoNavMapa

--- public/js/admin.js
+++ public/js/admin.js
@@ -64,7 +64,7 @@ const main = () => {
             map.addLayer({
                 id: "places2",
                 type: "circle",
-                source: "places",
+                source: "places2",
                 paint: {
                     "circle-color": "#4264fb",
                     "circle-radius": 6,
@@ -77,11 +77,40 @@ const main = () => {
         })
         .catch((err) => console.error(err));
 
+    axios
+        .get("http://localhost:4000/api/v1/puntos6")
+        .then((result) => {
+            // Add the data to the map as a source with ID 'places'
+            map.addSource("places3", {
+                type: "geojson",
+                data: result.data,
+            });
+
+            // Add a layer to the map
+            map.addLayer({
+                id: "places3",
+                type: "circle",
+                source: "places3",
+                paint: {
+                    "circle-color": "#4264fb",
+                    "circle-radius": 6,
+                    "circle-stroke-width": 2,
+                    "circle-stroke-color": "#ffffff",
+                },
+            });
+
+            console.log(result.data);
+        })
+        .catch((err) => console.error(err));
+
+
     // Create a popup, but don't add it to the map yet.
     const popup = new mapboxgl.Popup({
         closeButton: false,
         closeOnClick: false
     });
+
+    //MAP ONLOAD 1
 
     map.on('load', () => {
         // Add event listeners for mouseenter and mouseleave on the 'places' layer
@@ -114,7 +143,7 @@ const main = () => {
         });
     });
 
-    //MAP ONLOAD 
+    //MAP ONLOAD 2
 
     map.on('load', () => {
         // Add event listeners for mouseenter and mouseleave on the 'places' layer
@@ -147,13 +176,47 @@ const main = () => {
         });
     });
 
+    // MAP ONLOAD 3
+
+    map.on('load', () => {
+        // Add event listeners for mouseenter and mouseleave on the 'places' layer
+        map.on('mouseenter', 'places3', (e) => {
+            // Change the cursor style as a UI indicator.
+            map.getCanvas().style.cursor = 'pointer';
+
+            // Copy coordinates array.
+            const coordinates = e.features[0].geometry.coordinates.slice();
+            const description = ` ${e.features[0].properties.nombre}
+     <img src="${e.features[0].properties.img}">
+      Dirección: ${e.features[0].properties.Direccion}
+      Horario: ${e.features[0].properties.Horario}      `;
+
+            // Ensure that if the map is zoomed out such that multiple
+            // copies of the feature are visible, the popup appears
+            // over the copy being pointed to.
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+
+            // Populate the popup and set its coordinates
+            // based on the feature found.
+            popup.setLngLat(coordinates).setHTML(description).addTo(map);
+        });
+
+        map.on('mouseleave', 'places3', () => {
+            map.getCanvas().style.cursor = '';
+            popup.remove();
+        });
+    });
+
     // FUNCIONAMIENTO BOTONES
 
     // Get the button element by id
     const toggleMarkersButton = document.getElementById('toggleMarkers');
     const toggleMarkersButton2 = document.getElementById('toggleMarkers2');
+    const toggleMarkersButton3 = document.getElementById('toggleMarkers3');
 
-    // Add event listener to the button
+    // Add event listener to the button MUSEOS
     toggleMarkersButton.addEventListener('click', () => {
         // Check if the 'places' layer is currently visible
         const placesLayer = map.getLayer('places');
@@ -197,7 +260,7 @@ const main = () => {
         }
     });
 
-    // Add event listener to the button
+    // Add event listener to the button PLAZAS Y PARQUES
     toggleMarkersButton2.addEventListener('click', () => {
         // Check if the 'places2' layer is currently visible
         const placesLayer2 = map.getLayer('places2');
@@ -238,6 +301,50 @@ const main = () => {
 
             // Add the "active" class to the button
             toggleMarkersButton2.classList.add('active');
+        }
+    });
+
+    // Add event listener to the button SERVICIOS
+    toggleMarkersButton3.addEventListener('click', () => {
+        // Check if the 'places2' layer is currently visible
+        const placesLayer3 = map.getLayer('places3');
+        if (placesLayer3) {
+            // If the layer is visible, remove it from the map
+            map.removeLayer('places3');
+            map.removeSource('places3');
+
+            // Remove the "active" class from the button
+            toggleMarkersButton3.classList.remove('active');
+        } else {
+            // If the layer is not visible, fetch the GeoJSON data and add it to the map as a source and layer
+            axios
+                .get("http://localhost:4000/api/v1/puntos6")
+                .then((result) => {
+                    // Add the data to the map as a source with ID 'places2'
+                    map.addSource("places3", {
+                        type: "geojson",
+                        data: result.data,
+                    });
+
+                    // Add a layer to the map
+                    map.addLayer({
+                        id: "places3",
+                        type: "circle",
+                        source: "places3",
+                        paint: {
+                            "circle-color": "#4264fb",
+                            "circle-radius": 6,
+                            "circle-stroke-width": 2,
+                            "circle-stroke-color": "#ffffff",
+                        },
+                    });
+
+                    console.log(result.data);
+                })
+                .catch((err) => console.error(err));
+
+            // Add the "active" class to the button
+            toggleMarkersButton3.classList.add('active');
         }
     });
 
