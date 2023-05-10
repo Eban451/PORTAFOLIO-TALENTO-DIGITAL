56920ea200cb10187ce8ec9e51e5a372677198e1
Eban451 <eban316@gmail.com>
2023-05-09 22:45:18 -0400

    alertLogout

---
+++ public/js/alerts.js
@@ -0,0 +1,21 @@
+function showAlert() {
+    Swal.fire({
+        title: '¿Estás seguro?',
+        text: "Te vas a desconectar",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, desconectarme'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire(
+                'Te has desconectado!',
+                'Sesión cerrada correctamente',
+                'success'
+            ).then(function () {
+                window.location = '/users/logout';
+            });
+        }
+    });
+}
